dfdd55de3d223d9a743b6efe982d459fc5a7390e
daliaremila <dalia.remila@etu.umontpellier.fr>
2026-04-16 14:07:49 +0100

    creation des grilles dexemple

---
+++ data-raw/puzzles.R
@@ -0,0 +1,33 @@
+# Script de generation des grilles d'exemple
+# Execute une seule fois pour creer les fichiers dans data/
+# Regenerer si on change les grilles.
+
+# ---- Grille facile : 3 x 4 ----
+puzzle_easy <- slitherlinkr::new_puzzle(
+  matrix(c(NA, 3, NA, 2,
+           2, NA, 1, NA,
+           NA, 0, NA, 3), nrow = 3, byrow = TRUE)
+)
+
+# ---- Grille moyenne : 5 x 5 (exemple du sujet) ----
+puzzle_medium <- slitherlinkr::new_puzzle(
+  matrix(c(NA,  2,  2, NA, NA,
+           NA, NA, NA,  3,  2,
+           NA, NA, NA, NA,  1,
+           3,  0, NA, NA,  2,
+           NA,  3,  2,  2, NA), nrow = 5, byrow = TRUE)
+)
+
+# ---- Grille difficile : 7 x 7 ----
+puzzle_hard <- slitherlinkr::new_puzzle(
+  matrix(c(NA,  3, NA, NA,  2, NA, NA,
+           2, NA, NA,  1, NA, NA,  3,
+           NA, NA,  3, NA,  2, NA, NA,
+           1, NA, NA,  2, NA, NA,  2,
+           NA, NA,  3, NA,  1, NA, NA,
+           3, NA, NA,  2, NA, NA,  0,
+           NA, NA, NA, NA,  3,  2, NA), nrow = 7, byrow = TRUE)
+)
+
+# Sauvegarder dans data/ (genere data/puzzle_easy.rda, etc.)
+usethis::use_data(puzzle_easy, puzzle_medium, puzzle_hard, overwrite = TRUE)
