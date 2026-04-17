ede6e17569b6a8d21a19a29c7218bfd7bf1e369d
daliaremila <dalia.remila@etu.umontpellier.fr>
2026-04-17 15:48:59 +0100

    Refactor plot function for slitherlink puzzles

--- slitherlinkR/plot.R
+++ slitherlinkR/plot.R
@@ -1,43 +1,37 @@
-#' @export
-plot.slitherlink <- function(x) {
-  library(ggplot2)
-  n <- x$n; m <- x$m
+library(ggplot2)
+
+plot_puzzle <- function(p) {
+  if (is.null(p)) return(NULL)
+  n <- p$n; m <- p$m
   
-  # 1. Génération de toutes les arêtes possibles (Gris par défaut)
-  df_h <- expand.grid(y = 0:n, x_start = 0:(m-1))
-  df_v <- expand.grid(x = 0:m, y_start = 0:(n-1))
+  df_chiffres <- as.data.frame(which(!is.na(p$clues), arr.ind = TRUE))
+  if (nrow(df_chiffres) > 0) {
+    df_chiffres$val <- p$clues[!is.na(p$clues)]
+    df_chiffres$x <- df_chiffres$col - 0.5
+    df_chiffres$y <- n - df_chiffres$row + 0.5
+  }
+  
+  h_actifs <- which(p$h_edges == 1, arr.ind = TRUE)
+  df_h <- if(nrow(h_actifs) > 0) data.frame(x = h_actifs[,2]-1, xend = h_actifs[,2], y = n-h_actifs[,1]+1, yend = n-h_actifs[,1]+1) else data.frame()
+  
+  v_actifs <- which(p$v_edges == 1, arr.ind = TRUE)
+  df_v <- if(nrow(v_actifs) > 0) data.frame(x = v_actifs[,2]-1, xend = v_actifs[,2]-1, y = n-v_actifs[,1], yend = n-v_actifs[,1]+1) else data.frame()
+  
+  h_croix <- which(p$h_edges == -1, arr.ind = TRUE)
+  df_cx_h <- if(nrow(h_croix) > 0) data.frame(x = h_croix[,2]-0.5, y = n-h_croix[,1]+1) else data.frame()
+  
+  v_croix <- which(p$v_edges == -1, arr.ind = TRUE)
+  df_cx_v <- if(nrow(v_croix) > 0) data.frame(x = v_croix[,2]-1, y = n-v_croix[,1]+0.5) else data.frame()
   
   g <- ggplot() +
-    # Arêtes horizontales grises (fond)
-    geom_segment(data=df_h, aes(x=x_start, y=y, xend=x_start+1, yend=y), color="grey90") +
-    # Arêtes verticales grises (fond)
-    geom_segment(data=df_v, aes(x=x, y=y_start, xend=x, yend=y_start+1), color="grey90") +
-    # Points de la grille
-    geom_point(data=expand.grid(x=0:m, y=0:n), aes(x, y), size=2, color="grey30")
+    geom_tile(data = expand.grid(x = 1:m - 0.5, y = 1:n - 0.5), aes(x, y), fill = "#fdfdfd", color = "#eeeeee")
   
-  # 2. Ajout des traits noirs (valeur 1)
-  h_active <- which(x$h_edges == 1, arr.ind = TRUE)
-  if(nrow(h_active) > 0) {
-    df_ha <- data.frame(x=h_active[,2]-1, y=h_active[,1]-1, xend=h_active[,2], yend=h_active[,1]-1)
-    g <- g + geom_segment(data=df_ha, aes(x=x, y=y, xend=xend, yend=yend), color="black", linewidth=1.5)
-  }
+  if (nrow(df_h) > 0) g <- g + geom_segment(data = df_h, aes(x=x, y=y, xend=xend, yend=yend), color = "#00d2ff", size = 2.5, lineend = "round")
+  if (nrow(df_v) > 0) g <- g + geom_segment(data = df_v, aes(x=x, y=y, xend=xend, yend=yend), color = "#00d2ff", size = 2.5, lineend = "round")
+  if (nrow(df_cx_h) > 0) g <- g + geom_text(data = df_cx_h, aes(x, y), label = "×", color = "#ff4b2b", size = 8, fontface = "bold")
+  if (nrow(df_cx_v) > 0) g <- g + geom_text(data = df_cx_v, aes(x, y), label = "×", color = "#ff4b2b", size = 8, fontface = "bold")
+  if (nrow(df_chiffres) > 0) g <- g + geom_text(data = df_chiffres, aes(x, y, label = val), size = 12, fontface = "bold", color = "#2c3e50")
   
-  v_active <- which(x$v_edges == 1, arr.ind = TRUE)
-  if(nrow(v_active) > 0) {
-    df_va <- data.frame(x=v_active[,2]-1, y=v_active[,1]-1, xend=v_active[,2]-1, yend=v_active[,1])
-    g <- g + geom_segment(data=df_va, aes(x=x, y=y, xend=xend, yend=yend), color="black", linewidth=1.5)
-  }
-  
-  # 3. Croix rouges
-  h_cross <- which(x$h_edges == -1, arr.ind = TRUE)
-  if(nrow(h_cross) > 0) g <- g + geom_text(data=as.data.frame(h_cross), aes(x=col-0.5, y=row-1, label="x"), color="red")
-  
-  # 4. Chiffres (indices)
-  clues_df <- expand.grid(y = 0.5:(n-0.5), x = 0.5:(m-0.5))
-  clues_df$val <- as.vector(x$clues)
-  clues_df <- clues_df[!is.na(clues_df$val),]
-  g <- g + geom_text(data=clues_df, aes(x=x, y=y, label=val), size=6, fontface="bold") +
-    scale_y_reverse() + coord_fixed() + theme_void()
-  
-  return(g)
+  g + geom_point(data = expand.grid(x = 0:m, y = 0:n), aes(x, y), color = "#bdc3c7", size = 2) +
+    theme_void() + theme(panel.background = element_rect(fill = "white", color = NA))
 }
